b2da66a447820747b1102b3b3e561cc889ab0036
Thuy Dang Tran <thuy.dang@plenti.com.au>
2023-04-10 13:29:17 +0700

    FT-420: Create model ef core

--- FashionBrowser.Infrastucture/DataContext/AppDbContext.cs
+++ FashionBrowser.Infrastucture/DataContext/AppDbContext.cs
@@ -1,9 +1,22 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FashionBrowser.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EcommercyWeb.Infrastructure
 {
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<OrderDetail>().HasKey(key => new { key.OrderId, key.ProductId });
+        }
     }
 }
