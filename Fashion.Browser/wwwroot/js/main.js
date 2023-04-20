38d32484c11d3bdaf68e3928f18cfd3a70a0c263
Thai Dang Tran <dtranthai1989@gmail.com>
2023-04-20 08:55:35 +0700

    FT-429 show cart

--- Fashion.Browser/wwwroot/js/main.js
+++ Fashion.Browser/wwwroot/js/main.js
@@ -3,7 +3,7 @@ $(function() {
 
   //------- Parallax -------//
   skrollr.init({
-    forceHeight: false
+	forceHeight: false
   });
 
   //------- Active Nice Select --------//
@@ -11,59 +11,59 @@ $(function() {
 
   //------- hero carousel -------//
   $(".hero-carousel").owlCarousel({
-    items:3,
-    margin: 10,
-    autoplay:false,
-    autoplayTimeout: 5000,
-    loop:true,
-    nav:false,
-    dots:false,
-    responsive:{
-      0:{
-        items:1
-      },
-      600:{
-        items: 2
-      },
-      810:{
-        items:3
-      }
-    }
+	items:3,
+	margin: 10,
+	autoplay:false,
+	autoplayTimeout: 5000,
+	loop:true,
+	nav:false,
+	dots:false,
+	responsive:{
+	  0:{
+		items:1
+	  },
+	  600:{
+		items: 2
+	  },
+	  810:{
+		items:3
+	  }
+	}
   });
 
   //------- Best Seller Carousel -------//
   if($('.owl-carousel').length > 0){
-    $('#bestSellerCarousel').owlCarousel({
-      loop:true,
-      margin:30,
-      nav:true,
-      navText: ["<i class='ti-arrow-left'></i>","<i class='ti-arrow-right'></i>"],
-      dots: false,
-      responsive:{
-        0:{
-          items:1
-        },
-        600:{
-          items: 2
-        },
-        900:{
-          items:3
-        },
-        1130:{
-          items:4
-        }
-      }
-    })
+	$('#bestSellerCarousel').owlCarousel({
+	  loop:true,
+	  margin:30,
+	  nav:true,
+	  navText: ["<i class='ti-arrow-left'></i>","<i class='ti-arrow-right'></i>"],
+	  dots: false,
+	  responsive:{
+		0:{
+		  items:1
+		},
+		600:{
+		  items: 2
+		},
+		900:{
+		  items:3
+		},
+		1130:{
+		  items:4
+		}
+	  }
+	})
   }
 
   //------- single product area carousel -------//
   $(".s_Product_carousel").owlCarousel({
-    items:1,
-    autoplay:false,
-    autoplayTimeout: 5000,
-    loop:true,
-    nav:false,
-    dots:false
+	items:1,
+	autoplay:false,
+	autoplayTimeout: 5000,
+	loop:true,
+	nav:false,
+	dots:false
   });
 
   //------- mailchimp --------//  
@@ -74,43 +74,43 @@ $(function() {
   
   //------- fixed navbar --------//  
   $(window).scroll(function(){
-    var sticky = $('.header_area'),
-    scroll = $(window).scrollTop();
+	var sticky = $('.header_area'),
+	scroll = $(window).scrollTop();
 
-    if (scroll >= 100) sticky.addClass('fixed');
-    else sticky.removeClass('fixed');
+	if (scroll >= 100) sticky.addClass('fixed');
+	else sticky.removeClass('fixed');
   });
 
   //------- Price Range slider -------//
   if(document.getElementById("price-range")){
   
-    var nonLinearSlider = document.getElementById('price-range');
-    
-    noUiSlider.create(nonLinearSlider, {
-        connect: true,
-        behaviour: 'tap',
-        start: [ 500, 4000 ],
-        range: {
-            // Starting at 500, step the value by 500,
-            // until 4000 is reached. From there, step by 1000.
-            'min': [ 0 ],
-            '10%': [ 500, 500 ],
-            '50%': [ 4000, 1000 ],
-            'max': [ 10000 ]
-        }
-    });
+	var nonLinearSlider = document.getElementById('price-range');
+	
+	noUiSlider.create(nonLinearSlider, {
+		connect: true,
+		behaviour: 'tap',
+		start: [ 500, 4000 ],
+		range: {
+			// Starting at 500, step the value by 500,
+			// until 4000 is reached. From there, step by 1000.
+			'min': [ 0 ],
+			'10%': [ 500, 500 ],
+			'50%': [ 4000, 1000 ],
+			'max': [ 10000 ]
+		}
+	});
   
   
-    var nodes = [
-        document.getElementById('lower-value'), // 0
-        document.getElementById('upper-value')  // 1
-    ];
+	var nodes = [
+		document.getElementById('lower-value'), // 0
+		document.getElementById('upper-value')  // 1
+	];
   
-    // Display the slider value and how far the handle moved
-    // from the left edge of the slider.
-    nonLinearSlider.noUiSlider.on('update', function ( values, handle, unencoded, isTap, positions ) {
-        nodes[handle].innerHTML = values[handle];
-    });
+	// Display the slider value and how far the handle moved
+	// from the left edge of the slider.
+	nonLinearSlider.noUiSlider.on('update', function ( values, handle, unencoded, isTap, positions ) {
+		nodes[handle].innerHTML = values[handle];
+	});
   
   }
   
--- FashionBrowser.Utilities/Description.cs
+++ FashionBrowser.Utilities/Description.cs
@@ -10,4 +10,15 @@ namespace FashionBrowser.Utilities
 	{
 		public const string Cart_Key = "Cart";
 	}
+
+	public struct Mode
+	{
+		public const string MODE = "MODE";
+		public const string USING_MODAL_CONFIRM = "USING MODAL CONFIRM";
+		public const string USING_LABEL_CONFIRM = "USING LABEL CONFIRM";
+		public const string LABEL_CONFIRM_SUCCESS = "SUCCESS LABEL CONFIRM";
+		public const string LABEL_CONFIRM_FAIL = "FAIL LABEL CONFIRM";
+		public const string MODAL_CONFIRM_SUCCESS = "SUCCESS MODAL CONFIRM";
+		public const string MODAL_CONFIRM_FAIL = "FAIL MODAL CONFIRM";
+	}
 }
