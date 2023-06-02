2e9c97d17d9b377b91f5f4d0406f642fb636de1b
Thai Dang Tran <dtranthai1989@gmail.com>
2023-06-02 08:48:39 +0700

    FT-477 register user page

---
+++ FashionBrowser.Domain/Services/UserService.cs
@@ -0,0 +1,45 @@
+﻿
+using FashionBrowser.Domain.Model;
+using FashionBrowser.Domain.ViewModels;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FashionBrowser.Domain.Services
+{
+	public class UserService : IUserService
+	{
+        private readonly IUrlService _urlService;
+        private readonly HttpClient _httpClient;
+        public bool _isSuccess;
+
+        public UserService(IUrlService urlService, HttpClient httpClient)
+        {
+            _urlService = urlService;
+            _httpClient = httpClient;
+        }
+        public async Task<Tuple<bool, string>> RegisterUserAsync(UserItemViewModel registerUser)
+		{
+            try
+            {
+                var apiUrl = _urlService.GetBaseUrl() + "api/users/register";
+                var response = await _httpClient.PostAsJsonAsync(apiUrl, registerUser);
+
+                var responseList = JsonConvert.DeserializeObject<ResponseAPI<UserItemViewModel>>
+                                   (await response.Content.ReadAsStringAsync());
+                _isSuccess = responseList.IsSuccess;
+
+                return Tuple.Create(_isSuccess,  responseList.Message);
+            }
+            catch (Exception exception)
+            {               
+                return Tuple.Create(false, exception.Message);
+            }
+        }
+	}
+}
---
+++ FashionBrowser.Domain/ViewModels/UserItemViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FashionBrowser.Domain.ViewModels
+{
+	public class UserItemViewModel
+	{
+		[Required(ErrorMessage = "First name is required")]
+		public string FirstName { get; set; }
+
+		[Required(ErrorMessage = "Last name is required")]
+		public string LastName { get; set; }
+
+		[Required(ErrorMessage = "Email is required")]
+		public string Email { get; set; }
+
+		public string UserName { get; set; }
+
+		[Required]
+        public string Password { get; set; }
+
+		[Required]
+        public string ConfirmPassword { get; set; }
+
+		public string PhoneNumber { get; set; }
+	}
+}
